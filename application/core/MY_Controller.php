47ce18d8cd0e14429e1c90eaf1aac2ec1fa0c34a
w-chiang <williampwc@gmail.com>
2015-02-17 15:53:25 -0800

    added caboose view parameters to the core Controller

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -34,6 +34,11 @@ class Application extends CI_Controller {
         $this->data['menubar'] = $this->parser->parse('_menubar', $this->config->item('menu_choices'),true);
         $this->data['content'] = $this->parser->parse($this->data['pagebody'], $this->data, true);
 
+        //set view parameters from the Caboose library
+        $this -> data['caboose_styles'] = $this -> caboose -> styles();
+        $this -> data['caboose_scripts'] = $this -> caboose -> scripts();
+        $this -> data['caboose_trailings'] = $this -> caboose -> trailings();
+        
         // finally, build the browser page!
         $this->data['data'] = &$this->data;
         $this->parser->parse('_template', $this->data);
