fee3685ef630eb2b62d5c014123186ab478229ec
w-chiang <williampwc@gmail.com>
2015-02-17 16:33:57 -0800

    added a templated view for building a table of people and their quotes

---
+++ application/views/admin_list.php
@@ -0,0 +1,16 @@
+<table cols="" border="0">
+    <tr>
+        <th>ID</th>
+        <th>Author</th>
+        <th>Mugshot</th>
+        <th>Their Saying</th>
+    </tr>
+    {quotes}
+    <tr>
+        <td>{id}</td>
+        <td>{who}</td>
+        <td>{mug}</td>
+        <td>{what}</td>
+    </tr>
+    {/quotes}
+</table>
