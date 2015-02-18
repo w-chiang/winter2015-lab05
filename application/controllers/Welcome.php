000857e0cf4b6ef5095992150ef67b6edc92dedc
w-chiang <williampwc@gmail.com>
2015-02-17 16:21:46 -0800

    modify to select a random quote in index

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -21,7 +21,12 @@ class Welcome extends Application {
     function index()
     {
 	$this->data['pagebody'] = 'justone';    // this is the view we want shown
-	$this->data = array_merge($this->data, (array) $this->quotes->last());
+    
+    //randomize the quote displays
+    $choice = rand(1,$this->quotes->size());
+	$this->data = array_merge($this->data, (array) $this->quotes->get($choice));
+    
+    
     $this->caboose->needed('rating', 'hollywood');
     $this->data['average'] = ($this0>data['vote_count']>0)? (this->data['vote_total']/$this->data['vote_count']) : 0;
 	$this->render();
