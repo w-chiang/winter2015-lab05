8424090c2f50ac0d18682013f85d6a62c44debc9
w-chiang <williampwc@gmail.com>
2015-02-21 11:21:36 -0800

    added form and controller methods to create a new quote

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -4,7 +4,8 @@ class Admin extends Application {
 
     function __construct()
     {
-	parent::__construct();
+        parent::__construct();
+        $this->load->helper('formfields');
     }
 
     function index()
@@ -15,11 +16,39 @@ class Admin extends Application {
         $this->render();
     }
     
+    //add a new quotation
     function add()
     {
-        
+        $quote = $this->quotes->create();
+        $this->present($quote);
     }
 
+    //builds the form using formfields helper
+    function present($quote)
+    {
+        $this->data['fid'] = makeTextField('ID#', 'id', $quote->id);
+        $this->data['fwho'] = makeTextField('Author', 'who', $quote->who);
+        $this->data['fmug'] = makeTextField('Picture', 'mug', $quote->mug);
+        $this->data['fwhat'] = makeTextField('Quote', 'what', $quote->what);
+        $this->data['pagebody'] = 'quote_edit';
+        $this->data['fsubmit'] = makeSubmitButton('Process Quote', "Click here to validate the quote data", 'btn-success');
+        $this->render();
+    }
+    
+    //confirms quote form
+    function confirm()
+    {
+        $record = $this->quotes->create();
+        $record->id = $this->input->post('id');
+        $record->who = $this->input->post('who');
+        $record->mug = $this->input->post('mug');
+        $record->what = $this->input->post('what');
+        if (empty($record->id)) 
+            $this->quotes->add($record);
+        else
+            $this->quotes->update($record);
+        redirect('/admin');
+    }
 }
 
 /* End of file Welcome.php */
---
+++ application/views/quote_edit.php
@@ -0,0 +1,9 @@
+<div class="row">
+<form action="/admin/confirm" method="post">
+    {fid}
+    {fwho}
+    {fmug}
+    {fwhat}
+    {fsubmit}
+</form>
+</div>
