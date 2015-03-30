17262d0c333d026769ab6ba97fd7073d2e90323b
w-chiang <williampwc@gmail.com>
2015-03-29 18:28:32 -0700

    implemented receipt view

--- application/views/justone.php
+++ application/views/justone.php
@@ -1,7 +1,27 @@
 <div class="row">
-    <div class="span3"><img src="/data/{mug}" title="{who}"/></div>
-    <div class="span8 offset1">
-        <p class="lead">{what}</p><br/>
-        <p class="text-right">{who}</p>
-    </div>
+    <p>
+        File: {filename}</br>
+        Delivery Type: {type}</br>
+        Customer Name: {customer}</br>   
+        Special Instructions: {special} </br>
+    </p>
+    
+    <p> Order Total: <b>${ordertotal}</b> </p>
+    
+    <p>Order Details:</p>
+    
+    <ul>
+    {burgers}
+    <li>
+        Burger subtotal: ${subtotal}
+        <br/>Patty: {patty}
+        <br/>Cheeses: Top: {topcheese}. Bottom: {bottomcheese}
+        <br/>Toppings: {toppings} {type} {/toppings} 
+        <br/>Sauces: {sauces} {type} {/sauces}
+    </li>
+    {/burgers}
+    
+    </ol>
+    </ul>
+    
 </div>
